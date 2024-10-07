48e6c3b2b8866c6d1944de904b41f85cedbf0d71
JorgeMachado <jorgehenriqueq@gmail.com>
2024-10-07 14:57:28 -0300

    Adicionado Tipos de dados primitivos

---
+++ JavaScript/Tipos de dados primitivos/index.js
@@ -0,0 +1,13 @@
+//String(Texto), number(numero), undefined(não inicializa uma variavel), null, boolean, symbol
+
+const nome = 'luiz'; //string
+const nome1 = "Luiz"; //string
+const nome2 = `Luiz`; //string
+const num1 = 10; //number
+const num2 = 10.52; //number
+let nomeAluno; //undefined -> não aponta pra local nenhum na memória
+const sobrenomeAluno = null; //Nulo -> não aponta pra local nenhum na memória
+const boolean = true; // 2 opções, true ou false (valor lógico)
+const aprovado = true;
+
+console.log(typeof nome, nome);
