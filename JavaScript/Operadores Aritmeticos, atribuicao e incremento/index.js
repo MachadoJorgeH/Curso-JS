d941aaf73469ae0ef8176602bd0d2f8cef01bafc
JorgeMachado <jorgehenriqueq@gmail.com>
2024-10-21 15:15:35 -0300

    Adicionado aula sobre operadores aritmeticos, atribuicao e incremento

---
+++ JavaScript/Operadores Aritmeticos, atribuicao e incremento/index.js
@@ -0,0 +1,36 @@
+/** 
+ * Aritméticos
+ * + (Adição / Concatenação)
+ * Ex: const num1 = 5;
+ *     const num2 = 10;
+ *     console.log(num1 + num2);
+ * - (Subtração)
+ * Ex: const num1 = 5;
+ *     const num2 = 10;
+ *     console.log(num1 - num2);
+ * **(Potenciação)
+ * * Ex: const num1 = 5;
+ *     const num2 = 10;
+ *     console.log(num1 ** num2);
+ * % (Resto da divisão)
+ * * Ex: const num1 = 5;
+ *     const num2 = 10;
+ *     console.log(num1 % num2);
+ * ++ (incremento)
+ *  Ex:
+ *      let contador = 1;
+ *      contador++; //2
+ *      contador++; //3
+ *      contador++; //4
+ *      contador++; //5
+ *      console.log(contador);
+ * Para incrementar números acima de 1
+ * let contador = 0;
+ * contador += 2;
+ * contador += 2;
+ * console.log(contador);
+ *      
+ * --(descremento)
+ *      let contador = 10;
+ *      console.log(--contador);
+ * */ 
