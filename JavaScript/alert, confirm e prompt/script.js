9322c05b03209265b0b8dfaaf4bd2d685dd5070b
JorgeMachado <jorgehenriqueq@gmail.com>
2024-10-21 15:55:09 -0300

    Adicionado aula sobre alert confirm e prompt

---
+++ JavaScript/alert, confirm e prompt/script.js
@@ -0,0 +1,18 @@
+/*alert('Com a nossa mensagem.');*/
+
+/*confirm('Deseja realmente apagar?')
+const confirma = confirm('Deseja realmente apagar?');
+Agora a variavel confirma tem o valor clicado pelo usuário(True or False)
+*/
+
+/*prompt('Digite o seu nome:');
+let num1 = prompt('Digite um número');
+num1 = Number(num1);
+Agora a variavel num1 tem o valor digitado pelo usuário*/
+
+let num1 = prompt('Digite um número:');
+let num2 = prompt('Digite outro número');
+num1 = Number(num1);
+num2 = Number(num2);
+const resultado = num1 + num2;
+alert('O resultado da sua conta foi: ' + resultado);
