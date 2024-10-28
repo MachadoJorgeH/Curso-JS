55394c62c5701d71f81246ed29373c2b6b32c105
JorgeMachado <jorgehenriqueq@gmail.com>
2024-10-28 15:39:46 -0300

    Adicionado aula sobre Arrays Basico

---
+++ JavaScript/Arrays(Básico)/index.js
@@ -0,0 +1,35 @@
+const alunos = ['Luiz', 'Maria', 'João'];
+
+console.log(alunos)
+
+// delete alunos[1]; //deleta o index 1 do array
+//console.log(alunos[1]);
+
+/*const removido = alunos.shift(); //Remove a primeira casa do array
+console.log(removido);
+console.log(alunos);
+*/
+
+/*const removido = alunos.pop(); //remove o ultimo elemento do array
+console.log(removido); //mostra quem foi removido
+console.log(alunos); //mostra o array atualizado
+*/
+
+/*alunos.unshift('Luiza');//Adicionou a primeira casa do array
+alunos.unshift('Fábio');//Adicionou a primeira casa do array
+*/
+
+
+
+/*alunos.push('Luiza'); //Adicionou a ultima casa do array
+alunos.push('Fábio'); ////Adicionou a ultima casa do array
+*/
+
+/*console.log(alunos);
+console.log(alunos[0]);
+console.log(alunos[1]);
+console.log(alunos[2]);
+*/
+
+//alunos[0] = 'Eduardo';//Alterado a casa 0 do array
+//alunos[3] = 'Luiza'; //Adicionado a terceira casa do array
