3238e52aabdcb42090e4d0881a3ac565057df5cd
JorgeMachado <jorgehenriqueq@gmail.com>
2024-10-28 15:00:32 -0300

    Adicionado aula sobre o Objeto Math

---
+++ JavaScript/Objeto Math/index.js
@@ -0,0 +1,13 @@
+let num1 = 9.54578;
+//let num2 = Math.floor(num1); //Aredonda o número para baixo
+//let num2 = Math.ceil(num1); //Arredonda para cima
+//let num2 = Math.round(num1); //Arrendonda para o mais próximo
+
+//console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)); //Pega o maior número da sequencia
+//console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6)); //Pega o menor número da sequencia
+
+
+//const aleatorio = Math.random() * (10-5) + 5;//Pega um numero aleatorio, nesse exemplo entre 5 a 10
+//console.log(aleatorio)
+
+console.log(Math.pow(2,10)); //Eleva o número x ao y(x,y)
