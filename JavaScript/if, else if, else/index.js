3e85e860bb46145e6d833cd91b42c210b2fca5b9
JorgeMachado <jorgehenriqueq@gmail.com>
2024-11-04 20:58:27 -0300

    Adicionado aula sobre if else if else

---
+++ JavaScript/if, else if, else/index.js
@@ -0,0 +1,42 @@
+/* if pode ser usado sozinho
+sempre que utilizo a palavra else, preciso de um if antes
+Eu posso ter vários else ifs na checagem
+Só posso ter um else na checagem
+Podemos usar condições sem else if, utilizando apenas if e else
+*/
+
+
+
+
+/*const hora = 10;
+
+if (hora >= 0 && hora <= 12) {
+    console.log('Bom dia!');
+} else if (hora >= 12 && hora <= 18) {
+    console.log('Boa tarde!');
+} else {
+    console.log('Boa noite!');
+}
+*/
+
+/*const tenhoGrana = true;
+
+if (tenhoGrana) {
+    console.log('Vou sair de casa');
+} else {
+    console.log('Nao vou sair de casa');
+}
+
+*/
+
+const numero = 10;
+
+if (numero >= 0 && numero <= 5) {
+    console.log('Sim, o número é maior que 0 e menor que 5');
+} else if (numero >= 6 && numero <= 8) {
+    console.log('Sim, o número está entre 6 e 8');
+} else if (numero >= 9 && numero <= 11) {
+    console.log('Sim, o número está entre 9 e 11');
+} else {
+    console.log('Nao, o número não está entre 0 e 11');
+}    
