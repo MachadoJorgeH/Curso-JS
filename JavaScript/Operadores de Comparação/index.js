a9ef2dfcc1c886d02644e74cbb6533e8230a8ba7
JorgeMachado <jorgehenriqueq@gmail.com>
2024-11-04 15:36:26 -0300

    Adicionado aula sobre operadores de comparação

---
+++ JavaScript/Operadores de Comparação/index.js
@@ -0,0 +1,16 @@
+/*
+Operadores de comparação
+
+> (maior que)
+>= (maior ou igual a)
+< (menor que)
+<= (menor ou igual a)
+== (igual a) checa valor
+=== (igualdade estrita) checa valor e tipo
+!= (diferente de) checa valor
+!== (diferente estrito) checa valor e tipo
+
+
+*/
+
+console.log(1 > 0);
