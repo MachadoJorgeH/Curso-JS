4c9b56397e3ac9fba357b4d9be7e05555f1e7277
JorgeMachado <jorgehenriqueq@gmail.com>
2024-12-11 16:03:00 -0300

    Adicionado curso React Basico e projeto Lista de Tarefas

---
+++ REACT/listatarefas/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Altere para 'react-dom/client'
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root')); // Crie o root
+root.render(<App />); // Renderize a aplicação
